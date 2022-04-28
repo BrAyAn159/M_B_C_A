533e2b0e6b73d6192925bfd6f6a83ddd3377b77f
BrAyAn159 <cornejob797@gmail.com>
2022-04-28 14:12:47 -0500

    mi primer trabajo

---
+++ EjercicioJava.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+*ejerciciosJava
+*/
+public class EjerciciosJava{
+
+public static void main(String[] args) {
+       //2.15
+       //Definir Variables
+       double p, r=4, i;
+       //Datos de entrada
+       Scanner lt=new Scanner(System.in);
+       System.out.println("Ingrese valor de la intemnsidad:");
+       i=lt.nextDouble();
+       //Proceso
+       p=r*Math.pow(i, 2);
+   
+       //Datos de salida
+       System.out.println("La potencia es :"+p);
+}
+
+}
