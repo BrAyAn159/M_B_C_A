cd84394b17780ad54baf875b542a20dcedba7456
BrAyAn159 <cornejob797@gmail.com>
2022-05-03 17:52:59 +0000

    UPeU trabajo

---
+++ EstCondicional/EJERCICIO31.py
@@ -0,0 +1,25 @@
+#Declarar Variables
+edad:int
+mensaje:str
+#Datos de Entrada
+edad=int(input("Ingrese su edad:"))
+#Proceso y Datos de Salida
+if edad>=18:
+  mensaje=(f"Puede votar porque su edad es {edad
+  }")
+else:
+  mensaje=(f"No puede votar porque su edad es {edad
+ //Proceso
+
+  }
+#Datos de Salida
+print(mensaje)
+ String mensaje;
+} else{
+    mensaje="No puede votar por que su edad es "+edad;{
+
+
+    
+    }
+
+}
