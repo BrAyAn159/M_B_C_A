64bfdc9d377aed1caabb4c690532cd0c1a62c0f7
BrAyAn159 <104572298+BrAyAn159@users.noreply.github.com>
2022-05-09 16:29:15 -0500

    Add files via upload

---
+++ votacion.java
@@ -0,0 +1,10 @@
+Algoritmo elecciones
+	definir edad como entero
+	Escribir "Escribe tu edad"
+	Leer edad
+	Si edad >= 18 Entonces
+		Escribir "Puedes votar eres mayor de edad"
+	SiNo
+		Escribir "No puedes votar eres menor de edad"
+	FinSi
+FinAlgoritmo
