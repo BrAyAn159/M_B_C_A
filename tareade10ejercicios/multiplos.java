9548ab6bf67d4ca7b00874fec5becfcf28c4b329
BrAyAn159 <cornejob797@gmail.com>
2022-05-29 21:19:35 +0000

    trabajo

---
+++ tareade10ejercicios/multiplos.java
@@ -0,0 +1,12 @@
+package tareade10ejercicios;
+
+public class multiplos {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while(i <= 100) {
+          System.out.println(i);
+          i+=5;
+        }
+    }  
+}
