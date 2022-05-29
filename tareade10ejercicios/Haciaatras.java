543129ae8295beeacc0d9e0a4f2f0e5a5198b8c5
BrAyAn159 <cornejob797@gmail.com>
2022-05-29 21:13:09 +0000

    trabajo

---
+++ tareade10ejercicios/Haciaatras.java
@@ -0,0 +1,13 @@
+package tareade10ejercicios;
+
+/**
+ * Haciaatras
+ */
+public class Haciaatras {
+
+    public static void main(String[] args) {
+        for(int i = 320; i >= 160; i-=20) {
+          System.out.println(i);
+        }
+    }  
+}
